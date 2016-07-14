964a50013848f0b3a82bae34e4f3d8187b4e5d4c
Allan JEGU <allanjegu@gmail.com>
2016-07-14 09:03:29 +0200

    application files

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var server = express();
+
+server.get('/', function (req, res) {
+  res.send('Hello Express!');
+});
+
+server.listen(3000, function () {
+  console.log('Sample app listening on port 3000!');
+  console.log('http://localhost:3000/');
+});
